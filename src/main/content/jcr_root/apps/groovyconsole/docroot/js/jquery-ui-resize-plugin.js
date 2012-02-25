487f4dc9b5d5b24d166d183ddc8c4847611ed856
Mark Szumowski <mark@szumowski.net>
2012-02-24 22:47:47 -0600

    Style and resize js fixes for Firefox

--- src/main/content/jcr_root/apps/groovyconsole/docroot/js/jquery-ui-resize-plugin.js
+++ src/main/content/jcr_root/apps/groovyconsole/docroot/js/jquery-ui-resize-plugin.js
@@ -30,23 +30,43 @@ $.ui.plugin.add("resizable", "alsoResizeReverse", {
         },
 
         _alsoResizeReverse = function(exp, c) {
-            $(exp).each(function() {
-                var el = $(this), start = $(this).data("resizable-alsoresize-reverse"), style = {}, css = c && c.length ? c : ['width', 'height', 'top', 'left'];
+            if ($.browser.mozilla) {
+                // Firefox leaves the top in-place and resizes the bottom of an absolutely positioned bottom: 0 div,
+                // so we need to counter-adjust the top by delta-height
+                $(exp).each(function() {
+                    var el = $(this), start = $(this).data("resizable-alsoresize-reverse"), style = {}, css = c && c.length ? c : ['width', 'height', 'top', 'left'];
 
-                $.each(css || ['width', 'height', 'top', 'left'], function(i, prop) {
-                    var sum = (start[prop]||0) - (delta[prop]||0);
-                    if (sum && sum >= 0)
-                        style[prop] = sum || null;
+                    $.each(css || ['width', 'height', 'top', 'left'], function(i, prop) {
+                        var sum = (start[prop]||0) - (delta[prop]||0);
+                        if (prop == 'top') {
+                            var dh = (delta['height']||0);
+                            sum += dh;
+                        }
+                        if (sum && sum >= 0)
+                            style[prop] = sum || null;
+                    });
+                    
+                    el.css(style);
                 });
+            } else {
+                $(exp).each(function() {
+                    var el = $(this), start = $(this).data("resizable-alsoresize-reverse"), style = {}, css = c && c.length ? c : ['width', 'height', 'top', 'left'];
 
-                //Opera fixing relative position
-                if (/relative/.test(el.css('position')) && $.browser.opera) {
-                    self._revertToRelativePosition = true;
-                    el.css({ position: 'absolute', top: 'auto', left: 'auto' });
-                }
+                    $.each(css || ['width', 'height', 'top', 'left'], function(i, prop) {
+                        var sum = (start[prop]||0) - (delta[prop]||0);
+                        if (sum && sum >= 0)
+                            style[prop] = sum || null;
+                    });
 
-                el.css(style);
-            });
+                    //Opera fixing relative position
+                    if (/relative/.test(el.css('position')) && $.browser.opera) {
+                        self._revertToRelativePosition = true;
+                        el.css({ position: 'absolute', top: 'auto', left: 'auto' });
+                    }
+
+                    el.css(style);
+                });
+            }
         };
 
         if (typeof(o.alsoResizeReverse) == 'object' && !o.alsoResizeReverse.nodeType) {
