4f334f8ed214557ae7c67672cc731ce935e10feb
Mark Szumowski <mark@szumowski.net>
2012-03-04 14:11:22 -0600

    replaced load error console logging with alert

--- src/main/content/jcr_root/apps/groovyconsole/docroot/js/console-main.js
+++ src/main/content/jcr_root/apps/groovyconsole/docroot/js/console-main.js
@@ -20,7 +20,7 @@ loadScript = function(scriptPath) {
             }
         },
         failure: function(response, opts) {
-            console.log('server-side failure with status code ' + response.status);
+            alert('server-side failure with status code ' + response.status);
         }
     });
 }
