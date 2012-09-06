363e3d5d2fa851a6f2c84c6541fdbc8b57723c90
Mark Daugherty <lykorian@gmail.com>
2012-09-06 14:09:14 -0500

    test cleanup.

--- src/test/groovy/com/citytechinc/cqlibrary/groovyconsole/AbstractRepositorySpec.groovy
+++ src/test/groovy/com/citytechinc/cqlibrary/groovyconsole/AbstractRepositorySpec.groovy
@@ -1,6 +1,5 @@
 package com.citytechinc.cqlibrary.groovyconsole
 
-import com.citytechinc.cqlibrary.groovyconsole.builder.JcrBuilder
 import com.citytechinc.cqlibrary.groovyconsole.metaclass.GroovyConsoleMetaClassRegistry
 
 import javax.jcr.Node
@@ -11,6 +10,8 @@ import spock.lang.Shared
 import spock.lang.Specification
 
 abstract class AbstractRepositorySpec extends Specification {
+
+    static final def NODE_TYPES = ['sling', 'replication', 'tagging', 'core', 'dam']
 
     static class ShutdownThread extends Thread {
 
@@ -24,18 +25,12 @@ abstract class AbstractRepositorySpec extends Specification {
 
     @Shared session
 
-    @Shared jcrBuilder
-
     def setupSpec() {
         session = getRepository().loginAdministrative(null)
 
-        jcrBuilder = new JcrBuilder(session)
-
-        registerNodeType("/SLING-INF/nodetypes/sling.cnd")
-        registerNodeType("/SLING-INF/nodetypes/replication.cnd")
-        registerNodeType("/SLING-INF/nodetypes/tagging.cnd")
-        registerNodeType("/SLING-INF/nodetypes/core.cnd")
-        registerNodeType("/SLING-INF/nodetypes/dam.cnd")
+        NODE_TYPES.each { type ->
+            registerNodeType("/SLING-INF/nodetypes/$type.cnd")
+        }
     }
 
     def cleanupSpec() {
--- src/test/groovy/com/citytechinc/cqlibrary/groovyconsole/servlets/ScriptSavingServletTest.groovy
+++ src/test/groovy/com/citytechinc/cqlibrary/groovyconsole/servlets/ScriptSavingServletTest.groovy
@@ -18,26 +18,25 @@ class ScriptSavingServletTest extends AbstractRepositorySpec {
 
     @Shared servlet
 
+    @Shared script
+
     def setupSpec() {
         GroovyConsoleMetaClassRegistry.registerNodeMetaClass()
 
         servlet = new ScriptSavingServlet()
-
         servlet.session = session
+
+        def stream = this.class.getResourceAsStream("/$SCRIPT_NAME")
+
+        script = stream.text
+
+        stream.close()
     }
 
     def "save script"() {
         setup: "mock request with file name and script parameters"
-        def request = Mock(SlingHttpServletRequest)
-        def response = Mock(SlingHttpServletResponse)
-
-        def stream = this.class.getResourceAsStream("/$SCRIPT_NAME")
-        def script = stream.text
-
-        request.getParameter(ScriptSavingServlet.FILE_NAME_PARAM) >> SCRIPT_NAME
-        request.getParameter(ScriptSavingServlet.SCRIPT_CONTENT_PARAM) >> script
-
-        stream.close()
+        def request = mockRequest()
+        def response = mockResponse()
 
         and: "create console node"
         def consoleNode = session.rootNode.getOrAddNode(ScriptSavingServlet.CONSOLE_ROOT.substring(1))
@@ -48,22 +47,48 @@ class ScriptSavingServletTest extends AbstractRepositorySpec {
         servlet.doPost(request, response)
 
         then: "script node has been created"
-        scriptNodeCreated(consoleNode, SCRIPT_NAME, script)
+        scriptNodeCreated()
+    }
 
-        cleanup: "remove test nodes"
+    def "overwrite existing script"() {
+        setup: "mock request with file name and script parameters"
+        def request = mockRequest()
+        def response = mockResponse()
+
+        when: "post to servlet"
+        servlet.doPost(request, response)
+
+        then: "script node has been created"
+        scriptNodeCreated()
+
+        cleanup: "remove test node"
         session.getNode('/etc').remove()
         session.save()
     }
 
-    void scriptNodeCreated(consoleNode, fileName, script) {
+    def "missing console root node"() {
+        setup: "mock request with file name and script parameters"
+        def request = mockRequest()
+        def response = mockResponse()
+
+        when: "post to servlet"
+        servlet.doPost(request, response)
+
+        then: "exception getting console node"
+        thrown(RepositoryException)
+    }
+
+    void scriptNodeCreated() {
+        def consoleNode = session.getNode(ScriptSavingServlet.CONSOLE_ROOT)
+
         assert consoleNode.hasNode(ScriptSavingServlet.SCRIPT_FOLDER_REL_PATH)
 
         def folderNode = consoleNode.getNode(ScriptSavingServlet.SCRIPT_FOLDER_REL_PATH)
 
         assert folderNode.primaryNodeType.name == JcrConstants.NT_FOLDER
-        assert folderNode.hasNode(fileName)
+        assert folderNode.hasNode(SCRIPT_NAME)
 
-        def fileNode = folderNode.getNode(fileName)
+        def fileNode = folderNode.getNode(SCRIPT_NAME)
 
         assert fileNode.primaryNodeType.name == JcrConstants.NT_FILE
         assert fileNode.hasNode(JcrConstants.JCR_CONTENT)
@@ -75,21 +100,16 @@ class ScriptSavingServletTest extends AbstractRepositorySpec {
         assert contentNode.get(JcrConstants.JCR_DATA).stream.text == script
     }
 
-    def "missing console root node"() {
-        setup: "mock request with file name and script parameters"
+    def mockRequest() {
         def request = Mock(SlingHttpServletRequest)
-        def response = Mock(SlingHttpServletResponse)
 
         request.getParameter(ScriptSavingServlet.FILE_NAME_PARAM) >> SCRIPT_NAME
+        request.getParameter(ScriptSavingServlet.SCRIPT_CONTENT_PARAM) >> script
 
-        this.class.getResourceAsStream("/$SCRIPT_NAME").withStream {
-            request.getParameter(ScriptSavingServlet.SCRIPT_CONTENT_PARAM) >> it.text
-        }
+        request
+    }
 
-        when: "post to servlet"
-        servlet.doPost(request, response)
-
-        then: "exception getting console node"
-        thrown(RepositoryException)
+    def mockResponse() {
+        Mock(SlingHttpServletResponse)
     }
 }
