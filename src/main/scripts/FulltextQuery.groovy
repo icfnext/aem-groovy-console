90858f1e15ff733fc9e4a264f05e4cbb57b92347
David Steele <davidcsteele@gmail.com>
2013-08-29 10:09:20 -0500

    sample script using createQuery

---
+++ src/main/scripts/FulltextQuery.groovy
@@ -0,0 +1,15 @@
+def predicates = [path: "/content" ,
+    		  type:"cq:Page",
+			  fulltext: "geometrixx",
+              orderby: "@jcr:score,@jcr:created",
+              "orderby.index": "true",
+              "orderby.sort": "desc"]
+              
+def query = createQuery(predicates)
+query.setHitsPerPage(10)
+
+def result = query.getResult()
+println "${result.getTotalMatches()} hits. Execution time: ${result.getExecutionTime()}s \n--"
+result.getHits().each { hit ->
+	println "page : ${hit.getNode()}" 
+}
