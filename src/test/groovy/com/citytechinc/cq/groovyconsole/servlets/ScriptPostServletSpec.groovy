792588fec05a8ea078935385483d6212a37edd93
daniel.madejek <daniel.madejek@fullsix.com>
2014-01-09 09:53:08 +0100

    Logic separation. Now servlet runs service to execute groovy script. Thanks to this we can not only use servlet for executing scripts but also inject service (and use all nice feature the groovy console has. For example bindings, response as json etc)

--- src/test/groovy/com/citytechinc/cq/groovyconsole/servlets/ScriptPostServletSpec.groovy
+++ src/test/groovy/com/citytechinc/cq/groovyconsole/servlets/ScriptPostServletSpec.groovy
@@ -2,6 +2,8 @@ package com.citytechinc.cq.groovyconsole.servlets
 import com.citytechinc.cq.groovy.extension.services.OsgiComponentService
 import com.citytechinc.cq.groovyconsole.services.ConfigurationService
 import com.citytechinc.cq.groovyconsole.services.EmailService
+import com.citytechinc.cq.groovyconsole.services.GroovyRunService
+import com.citytechinc.cq.groovyconsole.services.impl.GroovyRunServiceImpl
 import com.day.cq.replication.Replicator
 import com.day.cq.search.QueryBuilder
 import groovy.json.JsonSlurper
@@ -14,17 +16,23 @@ class ScriptPostServletSpec extends AbstractServletSpec {
 
     @Shared servlet
 
+    @Shared service
+
     @Shared script
 
     def setupSpec() {
         servlet = new ScriptPostServlet()
 
-        servlet.replicator = Mock(Replicator)
-        servlet.componentService = Mock(OsgiComponentService)
-        servlet.bundleContext = Mock(BundleContext)
-        servlet.configurationService = Mock(ConfigurationService)
-        servlet.queryBuilder = Mock(QueryBuilder)
-        servlet.emailService = Mock(EmailService)
+        service = new GroovyRunServiceImpl();
+
+        service.replicator = Mock(Replicator)
+        service.componentService = Mock(OsgiComponentService)
+        service.bundleContext = Mock(BundleContext)
+        service.configurationService = Mock(ConfigurationService)
+        service.queryBuilder = Mock(QueryBuilder)
+        service.emailService = Mock(EmailService)
+
+        servlet.groovyRunService = service;
 
         script = getScriptAsString("Script")
     }
