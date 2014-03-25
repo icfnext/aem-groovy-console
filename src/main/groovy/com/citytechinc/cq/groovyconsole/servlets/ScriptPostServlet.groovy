93b9e49f548c9f923af14004b2c2ae182f15e074
Kai Rasmussen <kai.rasmussen@velir.com>
2014-03-25 08:58:57 -0400

    Refactor permission code

--- src/main/groovy/com/citytechinc/cq/groovyconsole/servlets/ScriptPostServlet.groovy
+++ src/main/groovy/com/citytechinc/cq/groovyconsole/servlets/ScriptPostServlet.groovy
@@ -1,11 +1,11 @@
 package com.citytechinc.cq.groovyconsole.servlets
-
 import com.citytechinc.cq.groovyconsole.services.ConfigurationService
 import com.citytechinc.cq.groovyconsole.services.GroovyConsoleService
 import groovy.json.JsonBuilder
 import org.apache.felix.scr.annotations.Reference
 import org.apache.felix.scr.annotations.sling.SlingServlet
 import org.apache.jackrabbit.api.JackrabbitSession
+import org.apache.jackrabbit.api.security.user.Authorizable
 import org.apache.jackrabbit.api.security.user.Group
 import org.apache.jackrabbit.api.security.user.UserManager
 import org.apache.sling.api.SlingHttpServletRequest
@@ -27,17 +27,22 @@ class ScriptPostServlet extends SlingAllMethodsServlet {
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws
         ServletException, IOException {
-	    def result
-	    if (hasPermission(request)) {
-		    result = consoleService.runScript(request)
-	    } else {
-		    result = [executionResult:"", outputText:"", stacktraceText: "Error - you do not have permission to use the groovy console"]
-	    }
+	    def result = getResult(request)
 
         response.contentType = "application/json"
 
         new JsonBuilder(result).writeTo(response.writer)
     }
+
+	private Map<String, String> getResult(SlingHttpServletRequest request) {
+		def result
+		if (hasPermission(request)) {
+			result = consoleService.runScript(request)
+		} else {
+			result = [executionResult: "", outputText: "", stacktraceText: "Error - you do not have permission to use the groovy console"]
+		}
+		result
+	}
 
 	private boolean hasPermission(SlingHttpServletRequest request) {
 		def allowedGroupId = configurationService.allowedGroup
@@ -45,10 +50,14 @@ class ScriptPostServlet extends SlingAllMethodsServlet {
 			return true
 		}
 
-		def session = (JackrabbitSession)request.resourceResolver.adaptTo(Session.class)
-		UserManager userManager = session.userManager
-		Group allowedGroup = userManager.getAuthorizable(allowedGroupId)
+		UserManager userManager = getUserManager(request)
+		Authorizable allowedGroup = userManager.getAuthorizable(allowedGroupId)
 		def currentUser = userManager.getAuthorizable(request.userPrincipal)
-		allowedGroup && allowedGroup.isMember(currentUser)
+		allowedGroup && allowedGroup.isGroup() && ((Group)allowedGroup).isMember(currentUser)
+	}
+
+	private UserManager getUserManager(SlingHttpServletRequest request) {
+		def session = (JackrabbitSession) request.resourceResolver.adaptTo(Session.class)
+		session.userManager
 	}
 }
