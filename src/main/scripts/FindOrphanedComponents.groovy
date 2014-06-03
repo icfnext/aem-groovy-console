c3d2bed08296ae631093bc05ca86be30814eba7b
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-06-02 21:49:05 -0500

    updated.

--- src/main/scripts/FindOrphanedComponents.groovy
+++ src/main/scripts/FindOrphanedComponents.groovy
@@ -4,10 +4,6 @@ def componentManager = resourceResolver.adaptTo(ComponentManager)
 
 def validResourceTypes = componentManager.components*.resourceType
 
-def result = []
-
-result.add(["Path", "Resource Type"])
-
 getPage("/content/geometrixx").recurse { page ->
     def content = page.node
 
@@ -16,10 +12,6 @@ getPage("/content/geometrixx").recurse { page ->
 
         if (resourceType && !validResourceTypes.contains(resourceType)) {
             println "component at path ${node.path} has invalid resource type = $resourceType"
-
-            result.add([node.path, resourceType])
         }
     }
 }
-
-// result
