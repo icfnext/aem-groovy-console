68ac145832dbba231ffc91d02cdefc5bf498b2b6
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-10-14 15:51:38 -0500

    removed table options.

--- src/main/groovy/com/citytechinc/aem/groovy/console/table/Table.groovy
+++ src/main/groovy/com/citytechinc/aem/groovy/console/table/Table.groovy
@@ -2,80 +2,43 @@ package com.citytechinc.aem.groovy.console.table
 
 class Table {
 
-    static class Options {
-
-        private boolean page = false
-
-        private boolean sort = true
-
-        boolean alternatingRowStyle = true
-
-        int pageSize = 10
-
-        int sortColumn = -1
-
-        boolean showRowNumber = true
-
-        String getPage() {
-            getEnabled(page)
-        }
-
-        String getSort() {
-            getEnabled(sort)
-        }
-
-        private static String getEnabled(boolean value) {
-            value ? "enable" : "disable"
-        }
-    }
-
     List<String> columns = []
 
     List<List<String>> rows = []
 
-    def options = new Options()
-
     void columns(String... columnNames) {
+        assert !columns, "columns are already defined"
+
         columns.addAll(columnNames)
     }
 
     void columns(List<String> columnNames) {
+        assert !columns, "columns are already defined"
+
         columns.addAll(columnNames)
     }
 
-    void row(String... data) {
-        rows.add(data as List)
+    void row(String... row) {
+        assert columns, "columns must be defined before adding a row"
+        assert row.length == columns.size(), "row data size does not match number of columns"
+
+        rows.add(row as List)
     }
 
-    void row(List<String> data) {
-        rows.add(data)
+    void row(List<String> row) {
+        assert columns, "columns must be defined before adding a row"
+        assert row.size() == columns.size(), "row data size does not match number of columns"
+
+        rows.add(row)
     }
 
     void rows(List<List<String>> rows) {
+        assert columns, "columns must be defined before adding rows"
+
+        rows.each { row ->
+            assert row.size() == columns.size(), "one or more row data sizes does not match number of columns"
+        }
+
         this.rows.addAll(rows)
-    }
-
-    void alternatingRowStyle(boolean alternatingRowStyle) {
-        this.options.alternatingRowStyle = alternatingRowStyle
-    }
-
-    void page(boolean page) {
-        this.options.page = page
-    }
-
-    void pageSize(int pageSize) {
-        this.options.pageSize = pageSize
-    }
-
-    void showRowNumber(boolean showRowNumber) {
-        this.options.showRowNumber = showRowNumber
-    }
-
-    void sort(boolean sort) {
-        this.options.sort = sort
-    }
-
-    void sortColumn(int sortColumn) {
-        this.options.sortColumn = sortColumn
     }
 }
