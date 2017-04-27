9ebed31154ac9489f660330789632211da2371b0
Marcin Bakowski <marcin.bakowski@cognifide.com>
2017-04-27 10:47:04 +0200

    AEM 6.3: Service user and service resolver added.Updated dependencies.

--- src/main/groovy/com/icfolson/aem/groovy/console/extension/impl/DefaultStarImportExtensionProvider.groovy
+++ src/main/groovy/com/icfolson/aem/groovy/console/extension/impl/DefaultStarImportExtensionProvider.groovy
@@ -1,7 +1,6 @@
 package com.icfolson.aem.groovy.console.extension.impl
 
 import com.icfolson.aem.groovy.console.api.StarImportExtensionProvider
-import com.icfolson.aem.groovy.console.constants.GroovyConsoleConstants
 import org.apache.felix.scr.annotations.Component
 import org.apache.felix.scr.annotations.Service
 
@@ -9,8 +8,19 @@ import org.apache.felix.scr.annotations.Service
 @Component(immediate = true)
 class DefaultStarImportExtensionProvider implements StarImportExtensionProvider {
 
+    private static final Set<String> DEFAULT_STAR_IMPORTS = [
+        "javax.jcr",
+        "org.apache.sling.api",
+        "org.apache.sling.api.resource",
+        "com.day.cq.search",
+        "com.day.cq.tagging",
+        "com.day.cq.wcm.api",
+        "com.day.cq.replication",
+        "groovy.json"
+    ] as Set
+
     @Override
     Set<String> getStarImports() {
-        GroovyConsoleConstants.DEFAULT_STAR_IMPORTS
+        DEFAULT_STAR_IMPORTS
     }
 }
